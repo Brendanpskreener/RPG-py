257d671294630cdbf365054840b7910358950896
Brendan Schreiner <brendanpschreiner@gmail.com>
2017-05-07 00:35:03 -0500

    Removed crlf

--- classes/item.py
+++ classes/item.py
@@ -4,9 +4,9 @@
 class Item:
     """Defines the Item class object."""
 
-    def __init__(self, name, type, description, prop):
+    def __init__(self, name, type, description, value):
         """I have no fucking clue."""
         self.name = name
         self.type = type
         self.description = description
-        self.prop = prop
+        self.value = value
